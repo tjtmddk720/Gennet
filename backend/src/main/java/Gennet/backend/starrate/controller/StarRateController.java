16aeca7d93f980f7b7a26fa8814d440731897275
MinsunCho-dev <cms081222@gmail.com>
2023-08-17 02:32:07 +0900

    feat: 별점 수정

--- backend/src/main/java/Gennet/backend/starrate/controller/StarRateController.java
+++ backend/src/main/java/Gennet/backend/starrate/controller/StarRateController.java
@@ -1,25 +1,47 @@
-//package Gennet.backend.starrate.controller;
-//
-//import Gennet.backend.starrate.dto.StarRateDto;
-//import Gennet.backend.starrate.service.StarRateService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/star")
-//public class StarRateController {
-//    private final StarRateService starRateService;
-//
-//    @Autowired
-//    public StarRateController(StarRateService starRateService) {
-//        this.starRateService = starRateService;
-//    }
-//
-//    // 사용자의 평균 별점 반환 API
-//    @GetMapping("/average/{memberId}")
-//    public ResponseEntity<Double> getAverageStarRateForMember(@PathVariable Long memberId) {
-//        Double averageStarRate = starRateService.calculateAverageStarRateForMember(memberId);
-//        return ResponseEntity.ok(averageStarRate);
-//    }
-//}
+package Gennet.backend.starrate.controller;
+
+import Gennet.backend.member.entity.Member;
+import Gennet.backend.starrate.dto.StarRateDto;
+import Gennet.backend.starrate.entity.StarRate;
+import Gennet.backend.starrate.service.StarRateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/star-rate")
+public class StarRateController {
+    private final StarRateService starRateService;
+
+    @Autowired
+    public StarRateController(StarRateService starRateService) {
+        this.starRateService = starRateService;
+    }
+
+    // 별점 설정 API
+    @PostMapping("/set-rating")
+    public ResponseEntity<Void> setStarRate(@RequestBody StarRateDto starRateDto) {
+        StarRate starRate = new StarRate();
+
+        Member member = new Member();
+        member.setMemberId(starRateDto.getMemberId());
+        starRate.setMember(member);
+
+        Member ratedMember = new Member();
+        ratedMember.setMemberId(starRateDto.getRatedMemberId());
+        starRate.setRatedMember(ratedMember);
+
+        starRate.setRating(starRateDto.getRating());
+
+        starRateService.createStarRate(starRate);
+
+        return ResponseEntity.ok().build();
+    }
+
+    // 별점 조회 API
+    @GetMapping("/get-rating/{starRateId}")
+    public ResponseEntity<Long> getStarRate(@PathVariable Long starRateId) {
+        long rating = starRateService.getRatingById(starRateId);
+        return ResponseEntity.ok(rating);
+    }
+}
--- backend/src/main/java/Gennet/backend/starrate/mapper/StarRateMapper.java
+++ backend/src/main/java/Gennet/backend/starrate/mapper/StarRateMapper.java
@@ -1,47 +1,31 @@
-//package Gennet.backend.starrate.mapper;
-//
-//import Gennet.backend.starrate.dto.StarRateDto;
-//import Gennet.backend.starrate.entity.StarRate;
-//import Gennet.backend.member.entity.Member;
-////import Gennet.backend.chattingroom.entity.ChattingRoom;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@Component
-//public class StarRateMapper {
-//    public StarRateDto toDto(StarRate starRate) {
-//        StarRateDto dto = new StarRateDto();
-//        dto.setStarRateId(starRate.getStarRateId());
-//        dto.setMemberId(starRate.getMember().getMemberId());
-////        dto.setChattingRoomId(starRate.getChattingRoom().getChattingRoomId());
-//        dto.setStarRate(starRate.getStarRate());
-//        return dto;
-//    }
-//
-//    public List<StarRateDto> toDtoList(List<StarRate> starRates) {
-//        return starRates.stream()
-//                .map(this::toDto)
-//                .collect(Collectors.toList());
-//    }
-//
-//    public StarRate toEntity(StarRateDto dto) {
-//        StarRate starRate = new StarRate();
-//        starRate.setStarRateId(dto.getStarRateId());
-//
-//        // Member 엔티티 설정
-//        Member member = new Member();
-//        member.setMemberId(dto.getMemberId()); // dto로부터 받은 값 사용
-//        starRate.setMember(member);
-////
-////        // ChattingRoom 엔티티 설정
-////        ChattingRoom chattingRoom = new ChattingRoom();
-////        chattingRoom.setChattingRoomId(dto.getChattingRoomId()); // dto로부터 받은 값 사용
-////        starRate.setChattingRoom(chattingRoom);
-//
-//        starRate.setStarRate(dto.getStarRate());
-//        return starRate;
-//    }
-//}
-//
+package Gennet.backend.starrate.mapper;
+
+import Gennet.backend.member.entity.Member;
+import Gennet.backend.starrate.dto.StarRateDto;
+import Gennet.backend.starrate.entity.StarRate;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface StarRateMapper {
+    StarRateMapper INSTANCE = Mappers.getMapper(StarRateMapper.class);
+
+    @Mapping(target = "member", expression = "java(toMember(starRateDto.getMemberId()))")
+    @Mapping(target = "ratedMember", expression = "java(toMember(starRateDto.getRatedMemberId()))")
+    StarRate starRateDtoToEntity(StarRateDto starRateDto);
+
+    default Member toMember(Long memberId) {
+        if (memberId == null) {
+            return null;
+        }
+        Member member = new Member();
+        member.setMemberId(memberId);
+        return member;
+    }
+
+//        // ChattingRoom 엔티티 설정
+//        ChattingRoom chattingRoom = new ChattingRoom();
+//        chattingRoom.setChattingRoomId(dto.getChattingRoomId()); // dto로부터 받은 값 사용
+//        starRate.setChattingRoom(chattingRoom);
+}
